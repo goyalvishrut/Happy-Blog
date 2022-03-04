56dd2ebc8b22b162554a9ed001f84b269708f49a
Vishrut Goyal <GOYALVISHRUT@GMAIL.COM>
2022-03-04 12:39:40 +0530

    Edit and admin login
    added admin login page
    edit button enabled for the admin login

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_mail import Mail
 import json
+
+from pyparsing import PositionToken
 
 local_server = True
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 
 app = Flask(__name__)
+app.secret_key = 'the random string'
 
 app.config.update(MAIL_SERVER='smtp.gmail.com',
                   MAIL_PORT='465',
                   MAIL_USE_SSL=True,
-                  MAIL_USERNAME = params['gmail_user'],
-                  MAIL_PASSWORD = params['gmail_password'])
+                  MAIL_USERNAME=params['gmail_user'],
+                  MAIL_PASSWORD=params['gmail_password'])
 
 mail = Mail(app)
 if (local_server):
@@ -34,8 +37,9 @@ class Contact(db.Model):
     message = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(25), nullable=False)
 
+
 class Posts(db.Model):
-    '''sr_no, title, slug,content, date,writer    '''
+    '''sr_no, title,subtitle, slug,content, date,writer,img_file    '''
     sr_no = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     subtitle = db.Column(db.String(100), nullable=False)
@@ -48,7 +52,7 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts=Posts.query.filter_by().all()[0:params['numbers_of_posts']]
+    posts = Posts.query.filter_by().all()[0:params['numbers_of_posts']]
     return render_template('index.html', params=params, posts=posts)
 
 
@@ -59,8 +63,27 @@ def about():
 
 @app.route("/post/<string:post_slug>", methods=['GET'])
 def post_route(post_slug):
-    post=Posts.query.filter_by(slug=post_slug).first()
+    post = Posts.query.filter_by(slug=post_slug).first()
     return render_template('post.html', params=params, post=post)
+
+@app.route("/edit/<string:post_sr_no>", methods=['GET','POST'])
+def edit_route(post_sr_no):
+    if ('user' in session and session['user'] == params['admin_username']):
+        if (request.method == 'POST'):
+            title_v = request.form.get('title')
+            subtitle_v = request.form.get('subtitle')
+            slug_v = request.form.get('slug')
+            content_v = request.form.get('content')
+            img_file_v = request.form.get('img_file')
+            writer_v = request.form.get('writer')
+            date_v=datetime.now()
+        
+            if(post_sr_no=='0'):
+                '''sr_no, title,subtitle, slug,content, date,writer,img_file    '''
+                post=Posts(title=title_v,subtitle=subtitle_v,slug=slug_v,content=content_v,date=date_v,writer=writer_v,img_file=img_file_v)
+                db.session.add(post)
+                db.session.commit()
+    return render_template('edit.html', params=params,post_sr_no=post_sr_no)
 
 
 @app.route("/contact", methods=['GET', 'POST'])
@@ -84,6 +107,24 @@ def contact():
                           recipients=[params['gmail_user']],
                           body=message_v + "\n" + phone_v)
     return render_template('contact.html', params=params)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if ('user' in session and session['user'] == params['admin_username']):
+        posts = Posts.query.all()
+        return render_template('dashboard.html', params=params, posts=posts)
+
+    if (request.method == 'POST'):
+        # Admin Login
+        admin_username_v = request.form.get('admin_username')
+        admin_password_v = request.form.get('admin_password')
+        if (admin_username_v == params['admin_username'] and admin_password_v == params['admin_password']):
+            session['user'] = admin_username_v
+            posts = Posts.query.all()
+            return render_template('dashboard.html', params=params, posts=posts)
+            
+    return render_template('login.html', params=params)
 
 
 app.run(debug=True)
